1cf8a0501b38419cde519f814427755dccf7d38a
Colin Quelle <cquelle3@binghamton.edu>
2020-10-26 21:50:18 -0400

    Added adjustments to printing and output

--- submission.cpp
+++ submission.cpp
@@ -6,45 +6,48 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
-bool sorttuple(const tuple<int, int, float>& a, const tuple<int, int, float>& b){
+bool sorttuple(const tuple<int, int, float, int>& a, const tuple<int, int, float, int>& b){
     return (get<2>(a) > get<2>(b));
 }
 
-pair<int, vector<tuple<int, int, float>>> greedy1(vector<tuple<int, int, float>> val, int capacity){
+pair<int, vector<int>> greedy1(vector<tuple<int, int, float, int>> val, int capacity){
     
     int current_capacity = 0;
     int max_profit = 0;
-    vector<tuple<int, int, float>> ret;
+    vector<int> ret;
     
-    vector<tuple<int, int, float>> v = val;
+    vector<tuple<int, int, float, int>> v = val;
     sort(v.begin(), v.end(), sorttuple);
     
     for(int i = 0; i < int(v.size()); i++){
         if(current_capacity + get<0>(v[i]) <= capacity){
             max_profit += get<1>(v[i]);
             current_capacity += get<0>(v[i]);
-            ret.push_back(v[i]);
+            ret.push_back(get<3>(v[i]) + 1);
         }
     }
     
-    pair<int, vector<tuple<int, int, float>>> p = make_pair(max_profit, ret);
+    sort(ret.begin(), ret.end());
+    
+    pair<int, vector<int>> p = make_pair(max_profit, ret);
     
     return p;
 }
 
-pair<int, vector<tuple<int, int, float>>> greedy2(vector<tuple<int, int, float>> val, int capacity){
+pair<int, vector<int>> greedy2(vector<tuple<int, int, float, int>> val, int capacity){
     
     int max_profit = 0;
     int pmax = 0;
     
-    pair<int, vector<tuple<int, int, float>>> greedy1pair = greedy1(val, capacity);
+    pair<int, vector<int>> greedy1pair = greedy1(val, capacity);
     
     int greedy1_prof = get<0>(greedy1pair);
     
-    vector<tuple<int, int, float>> v = val;
+    vector<tuple<int, int, float, int>> v = val;
     sort(v.begin(), v.end(), sorttuple);
     
     int temp_index;
@@ -62,10 +65,11 @@ pair<int, vector<tuple<int, int, float>>> greedy2(vector<tuple<int, int, float>>
     else{
         max_profit = pmax;
         get<1>(greedy1pair).clear();
-        get<1>(greedy1pair).push_back(v[temp_index]);
+        get<1>(greedy1pair).push_back(get<3>(v[temp_index]) + 1);
+        sort(get<1>(greedy1pair).begin(), get<1>(greedy1pair).end());
     }
     
-    pair<int, vector<tuple<int, int, float>>> p = make_pair(max_profit, get<1>(greedy1pair));
+    pair<int, vector<int>> p = make_pair(max_profit, get<1>(greedy1pair));
     
     return p;
 }
@@ -126,18 +130,17 @@ void knapsack(int i, int profit, int weight, int capacity, int * maxprofit, int 
     }
 }
 
-pair<int, vector<tuple<int, int, float>>> bt_knapsack(vector<tuple<int, int, float>> val, int capacity){
+pair<int, vector<int>> bt_knapsack(vector<tuple<int, int, float, int>> val, int capacity){
     
-    pair<int, vector<tuple<int, int, float>>> greedy2pair = greedy2(val, capacity);
+    pair<int, vector<int>> greedy2pair = greedy2(val, capacity);
     
     int maxprofit = get<0>(greedy2pair);
     int og_maxprofit = maxprofit;
-    vector<tuple<int, int, float>> best_set = get<1>(greedy2pair);
+    vector<int> best_set = get<1>(greedy2pair);
     int numbest = 0;
     
-    vector<tuple<int, int, float>> v = val;
+    vector<tuple<int, int, float, int>> v = val;
     sort(v.begin(), v.end(), sorttuple);
-    
     
     int arr_size = v.size() + 1;
     int w[arr_size];
@@ -155,17 +158,18 @@ pair<int, vector<tuple<int, int, float>>> bt_knapsack(vector<tuple<int, int, flo
     knapsack(0, 0, 0, capacity, &maxprofit, &numbest, bestset, include, p, w, arr_size);
 
     if(maxprofit <= og_maxprofit){
-        pair<int, vector<tuple<int, int, float>>> ret = make_pair(maxprofit, best_set);
+        pair<int, vector<int>> ret = make_pair(maxprofit, best_set);
         return ret;
     }
     else{
         best_set.clear();
         for(int i = 1; i < arr_size; i++){
             if(bestset[i] == "yes"){
-                best_set.push_back(make_tuple(w[i], p[i], float(p[i]/w[i])));
+                best_set.push_back(get<3>(v[i-1]) + 1);
             }
         }
-        pair<int, vector<tuple<int, int, float>>> ret = make_pair(maxprofit, best_set);
+        sort(best_set.begin(), best_set.end());
+        pair<int, vector<int>> ret = make_pair(maxprofit, best_set);
         return ret;
     } 
 }
@@ -184,8 +188,9 @@ int main(int argc, char *argv[]) {
 
     int count = 0;
     int capacity = 0;
+    int input_size = 0;
     
-    vector<tuple<int, int, float>> vals;
+    vector<tuple<int, int, float, int>> vals;
     
     while(getline(infile, line)){
         
@@ -193,47 +198,51 @@ int main(int argc, char *argv[]) {
         s >> s1 >> s2;
         
         if(count == 0){
+            input_size = stoi(s1);
             count = stoi(s1);
             capacity = stoi(s2);
         }
         else{
-            vals.push_back(make_tuple(stoi(s1), stoi(s2), stof(s2)/stof(s1)));
+            vals.push_back(make_tuple(stoi(s1), stoi(s2), stof(s2)/stof(s1), input_size - count));
             count--;
             
             if(count == 0){
                 if(mode == 0){
-                    pair<int, vector<tuple<int, int, float>>> p = greedy1(vals, capacity);
-                    int max = get<0>(p);
-                    cout << "Max Profit Greedy Algorithm 1: " << max << endl;
+                    auto start = chrono::high_resolution_clock::now();
+                    pair<int, vector<int>> p = greedy1(vals, capacity);
+                    auto end = chrono::high_resolution_clock::now();
+                    auto time_needed = chrono::duration_cast<chrono::microseconds>(end - start).count();
                     
-                    cout << "Best Set: " << endl;
-                    vector<tuple<int, int, float>> v = get<1>(p);
+                    cout << input_size << " " << get<0>(p) << " " << time_needed << " ";
+                    vector<int> v = get<1>(p);
                     for(int i = 0; i < int(v.size()); i++){
-                        cout << get<0>(v[i]) << ", " << get<1>(v[i]) << endl;
+                        cout << v[i] << " ";
                     }
                     cout << endl;
                 }
                 else if(mode == 1){
-                    pair<int, vector<tuple<int, int, float>>> p = greedy2(vals, capacity);
-                    int max = get<0>(p);
-                    cout << "Max Profit Greedy Algorithm 2: " << max << endl;
-                    
-                    cout << "Best Set: " << endl;
-                    vector<tuple<int, int, float>> v = get<1>(p);
+                    auto start = chrono::high_resolution_clock::now();
+                    pair<int, vector<int>> p = greedy2(vals, capacity);
+                    auto end = chrono::high_resolution_clock::now();
+                    auto time_needed = chrono::duration_cast<chrono::microseconds>(end - start).count();
+
+                    cout << input_size << " " << get<0>(p) << " " << time_needed << " ";
+                    vector<int> v = get<1>(p);
                     for(int i = 0; i < int(v.size()); i++){
-                        cout << get<0>(v[i]) << ", " << get<1>(v[i]) << endl;
+                        cout << v[i] << " ";
                     }
                     cout << endl;
                 }
                 else{
-                    pair<int, vector<tuple<int, int, float>>> p = bt_knapsack(vals, capacity);
-                    int max = get<0>(p);
-                    cout << "Max Profit Backtracking Algorithm: " << max << endl;
-                    
-                    cout << "Best Set: " << endl;
-                    vector<tuple<int, int, float>> v = get<1>(p);
+                    auto start = chrono::high_resolution_clock::now();
+                    pair<int, vector<int>> p = bt_knapsack(vals, capacity);
+                    auto end = chrono::high_resolution_clock::now();
+                    auto time_needed = chrono::duration_cast<chrono::microseconds>(end - start).count();
+
+                    cout << input_size << " " << get<0>(p) << " " << time_needed << " ";
+                    vector<int> v = get<1>(p);
                     for(int i = 0; i < int(v.size()); i++){
-                        cout << get<0>(v[i]) << ", " << get<1>(v[i]) << endl;
+                        cout << v[i] << " ";
                     }
                     cout << endl;
                 }
